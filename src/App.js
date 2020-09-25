8444abc43afdba1745e11789124b982543b453be
jose-Olvera <abraham.jolvera@gmail.com>
2020-09-25 02:06:15 -0700

    users fetched from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import "./App.css";
 
 import NavBar from "./components/layout/NavBar";
 import Users from "./components/users/Users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users')
+    console.log(res.data);
+
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className="App">
         <NavBar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,24 +1,27 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const UserItem = (props) => {
-    const { login, avatar_url, html_url } = props.user;
-
-    return (
-      <div className="card text-center">
-        <img
-          alt="user icon"
-          className="round-img"
-          style={{ width: "60px" }}
-          src={avatar_url}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        alt="user icon"
+        className="round-img"
+        style={{ width: "60px" }}
+        src={avatar_url}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
+    </div>
+  );
+};
+
+UserItem.prototypes = {
+  user: PropTypes.object.isRequired
 }
 
 export default UserItem;
