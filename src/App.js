84ffce8f2f5aeaa44074a6af1c3e6d607bab0c3c
jose-Olvera <abraham.jolvera@gmail.com>
2020-09-25 02:51:19 -0700

    proptypes to app.js component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import React, { Component } from "react";
 import axios from "axios";
+import PropTypes from "prop-types";
 import "./App.css";
 
 import NavBar from "./components/layout/NavBar";
 import Users from "./components/users/Users";
-import Search from './components/users/Search';
+import Search from "./components/users/Search";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
   };
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
 
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLEINT_SECRET}`
-  //   );
-  //   console.log(res.data);
-
-  //   this.setState({ users: res.data, loading: false });
-  // }
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
 
   // Search Github Users
   searchUsers = async (text) => {
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLEINT_SECRET}`
     );
     console.log(res.data);
 
     this.setState({ users: res.data.items, loading: false });
-  }
+  };
 
   render() {
     return (
